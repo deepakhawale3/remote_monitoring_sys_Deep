208767e48d4789bb868871f09fe45d21d3714b40
deepakhawale3 <deepakhawale3@gmail.com>
2020-11-20 14:20:35 +0530

    Add files via upload

---
+++ RemoteClient/src/remoteclient/EnumCommands.java
@@ -0,0 +1,25 @@
+
+
+
+package remoteclient;
+
+/**
+ * Used to represent commands which can be sent by the server
+ */
+public enum EnumCommands {
+    PRESS_MOUSE(-1),
+    RELEASE_MOUSE(-2),
+    PRESS_KEY(-3),
+    RELEASE_KEY(-4),
+    MOVE_MOUSE(-5);
+
+    private int abbrev;
+
+    EnumCommands(int abbrev){
+        this.abbrev = abbrev;
+    }
+    
+   public int getAbbrev(){
+        return abbrev;
+    }
+}
